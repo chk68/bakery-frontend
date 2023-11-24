3b102b431f81257e5de1fdea219ffb2dd51f4d2e
Kirill <money.88887777@gmail.com>
2023-11-24 23:13:02 +0200

    Changed mobile-menu.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,24 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const mobileMenuLinks = document.querySelectorAll('.mobile-menu__link');
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => {
+    mobileMenu.classList.toggle('is-open');
+    disableScroll();
+};
+
+const disableScroll = () => {
+    document.body.classList.toggle('is-scroll-disabled');
+};
+
+const closeMenuOnClick = () => {
+    toggleMenu();
+};
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', closeMenuOnClick);
+});
